d0b54f7e003e82732e4f128c23524b4626dd40c0
Chris Thomson <chris@cthomson.ca>
2013-08-05 17:54:35 -0400

    Style tweaks on PLL algs page and footer.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   $(".alg-list li").click(function(ev) {
-    $alg = $(ev.target).closest("li");
-    $("#active-alg").fadeOut('fast', function() {
+    var $alg = $(ev.target).closest("li").clone();
+    $alg.find(".details h3").prepend($alg.find(".alg-name").text() + ": ")
+    $alg.find(".alg-name").remove()
+
+    if ($("#active-alg").is(":visible")) {
+      $("#active-alg").fadeOut('fast', function() {
+
        $("#active-alg").html($alg.html()).fadeIn();
-    });
+      });
+    } else {
+      $("#active-alg").html($alg.html()).slideDown('fast');
+    }
   });
 });
