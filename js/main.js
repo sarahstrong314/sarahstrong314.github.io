753610389532f324e476b9d707ea12f10503a2b6
Chris Thomson <chris@cthomson.ca>
2013-08-05 21:27:33 -0400

    Added in PLL algorithms content.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $(".alg-list li").click(function(ev) {
-    var $alg = $(ev.target).closest("li").clone();
+    var $alg = $(ev.target).closest("li");
+    $(".alg-list li").not($alg).find(".overview").css({opacity: 0.66})
+    $alg.find(".overview").css({opacity: 1.0})
+    $alg = $alg.clone();
+
     $alg.find(".details h3").prepend($alg.find(".alg-name").text() + ": ")
     $alg.find(".alg-name").remove()
 
