4cf2c0aa2ff6f9609fcb2f8f94660828c78a13dc
Chris Thomson <chris@cthomson.ca>
2013-08-17 18:20:31 -0400

    Support for sub-views on the algorithms pages.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
+
+  // Focussing on one particular algorithm.
   $(".alg-list li").click(function(ev) {
     var $alg = $(ev.target).closest("li");
     var activeAlgPosition;
 
-    $(".alg-list li").not($alg).find(".overview").css({opacity: 0.66})
-    $alg.find(".overview").css({opacity: 1.0})
+    $(".alg-list li").not($alg).find(".overview").addClass('inactive');
+    $alg.find(".overview").removeClass('inactive');
     $alg = $alg.clone();
 
     if ($alg.find(".alg-name").length > 0) {
@@ -28,4 +30,39 @@ $(document).ready(function() {
       $('body').animate({scrollTop: activeAlgPosition}, 500);
     }
   });
+
+  // Switching sub-views of algorithms.
+  $(".tabs li").click(function(ev) {
+    var $li = $(ev.target).closest("li");
+    var $activeTab = $(".tabs li.active")
+    var $activeList;
+
+    if ($li !== $activeTab) {
+      $("#active-alg").slideUp('fast', function() {
+        $("#active-alg").empty();
+        $(".inactive").removeClass('inactive');
+      });
+
+      if ($activeTab.length > 0) {
+        $activeList = $(".alg-list#" + $activeTab.attr('id'));
+      }
+
+      if ($activeList !== undefined) {
+        $activeList.fadeOut('fast', function() {
+          $(".alg-list#" + $li.attr('id')).fadeIn('fast');
+        });
+      } else {
+        $(".alg-list#" + $li.attr('id')).fadeIn('fast');
+      }
+
+      $(".tabs li").removeClass('active');
+      $li.addClass('active');
+
+      window.location.hash = $li.attr('id');
+    }
+  });
+
+  if ($('.collapsible').length > 0 && document.location.hash !== "") {
+    $(".tabs li" + document.location.hash).click();
+  }
 });
