5be0a8ef9cb311ac94cb116b83e9c9fbc5dee7c5
Chris Thomson <chris@cthomson.ca>
2013-08-19 00:29:33 -0400

    Modify deploy script ever so slightly...

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 jekyll build
 rsync -vr _site/* sa967st@cubing.net:sarah.cubing.net
-ssh sa967st@cubing.net chmod -R 777 sarah.cubing.net/images
+ssh sa967st@cubing.net chmod -R 777 sarah.cubing.net/images/
