5656aeae1331d93fb987184dbccf8195b424800c
Chris Thomson <chris@cthomson.ca>
2013-08-29 23:49:52 -0400

    Add robots.txt, but don't deploy it.
    We want it to be applied on sa967st.github.io but not sarah.cubing.net.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 jekyll build
 rsync -vr _site/* sa967st@cubing.net:sarah.cubing.net
-ssh sa967st@cubing.net chmod -R 777 sarah.cubing.net/images/
+ssh sa967st@cubing.net <<'END'
+chmod -R 777 sarah.cubing.net/images/
+rm sarah.cubing.net/robots.txt
+END
