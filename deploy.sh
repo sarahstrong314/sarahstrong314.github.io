ee915a6903c3f449fc25c5919d17700a55bc3b3a
Chris Thomson <chris@cthomson.ca>
2017-01-14 15:26:08 -0500

    Bump ruby version to 2.3.3 + other deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 bundle exec jekyll build
-rsync -vr _site/* sa967st@cubing.net:sarah.cubing.net
-ssh sa967st@cubing.net <<'END'
+rsync -vr _site/* sa967st@towns.dreamhost.com:sarah.cubing.net
+ssh sa967st@towns.dreamhost.com <<'END'
 chmod -R 777 sarah.cubing.net/images/
 rm sarah.cubing.net/robots.txt
 END
