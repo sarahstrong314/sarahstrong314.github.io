5a94be92a115f44f31652b1470e79a5ca75db0f0
Chris Thomson <chris@cthomson.ca>
2024-11-03 06:21:52 -0500

    Remove deploy script

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-bundle exec jekyll build
-rsync -vr _site/* sa967st@towns.dreamhost.com:sarah.cubing.net
-ssh sa967st@towns.dreamhost.com <<'END'
-chmod -R 777 sarah.cubing.net/images/
-rm sarah.cubing.net/robots.txt
-END
